af825d19ccb687215b18171b5b4d91d26742bbe0
Felipe Fagua <felipe.fagua@unity3d.com>
2025-03-26 18:08:59 -0500

    Updating the /libs Folder

--- untitled.cpp
+++ untitled.cpp
@@ -1,3 +1,29 @@
-int CountWordsInSentence(const char* sentence);
-void ReverseString(char *str, int begin, int end);
 
+
+//Updating the /libs Folder
+
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
+
+
+int main() {
+    // Simply tries to use the Lua language with the Sol library
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+
+    // Tries to create a vector2 and normalize it with the GLM library
+    glm::vec2 velocity = glm::vec2(5.0, -2.5);
+    velocity = glm::normalize(velocity);
+
+    // Tries to initialize SDL
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    std::cout << "Yay! Dependencies work correctly." << std::endl;
+    return 0;
+}
