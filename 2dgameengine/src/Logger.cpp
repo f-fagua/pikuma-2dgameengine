8c4fdddb5ec045375ca98c3f99fd86c83800e164
Felipe Fagua <felipe.fagua@unity3d.com>
2025-03-29 15:55:11 -0500

    Logger Class Implementation

--- 2dgameengine/src/Logger.cpp
+++ 2dgameengine/src/Logger.cpp
@@ -1,30 +1,33 @@
 #include "Logger.h"
 #include <iostream>
-#include <time.h>
+#include <string>
+#include <chrono>
 #include <ctime>
 
+std::vector<LogEntry> Logger::messages;
+
+std::string CurrentDateTimeToString() {
+	std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	std::string output(30, '\0');
+	std::strftime(&output[0], output.size(), "%d-%b-%Y %T", std::localtime(&now));
+	return output;
+}
+
 void Logger::Log(const std::string& message){
-	time_t rawtime;
-	struct tm* timeinfo;
-	char buffer[80];
+	LogEntry logEntry;
+	logEntry.type = LOG_INFO;
+	logEntry.message = "LOG: [" + CurrentDateTimeToString() + "]: " + message;
+	
+	std::cout << "\x1B[32m" << logEntry.message << "\033[0m" << std::endl;	
 
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);
-
-	strftime(buffer, 80, "LOG | %d-%h-%G %T - ", timeinfo);
-
-	std::cout << "\033[1;32m" << buffer << message << "\033[m" << std::endl;
+	messages.push_back(logEntry);
 }
 
 void Logger::Err(const std::string& message){
-	time_t rawtime;
-	struct tm* timeinfo;
-	char buffer[80];
-
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);
-
-	strftime(buffer, 80, "ERR | %d-%h-%G %T - ", timeinfo);
-
-	std::cout << "\033[1;31m" << buffer << message << "\033[m" << std::endl;
+	LogEntry logEntry;
+	logEntry.type = LOG_ERROR;
+	logEntry.message = "ERR: [" + CurrentDateTimeToString() + "]: " + message;
+	
+	std::cout << "\x1B[91m" << logEntry.message << "\033[0m" << std::endl;
+	messages.push_back(logEntry);
 }
