2ccb999501adaf4a886b440e2765d476db8c0faa
Felipe Fagua <felipe.fagua@unity3d.com>
2025-03-29 15:20:14 -0500

    Exercise: Logging Game Info

--- 2dgameengine/src/Logger.cpp
+++ 2dgameengine/src/Logger.cpp
@@ -1,9 +1,30 @@
 #include "Logger.h"
+#include <iostream>
+#include <time.h>
+#include <ctime>
 
 void Logger::Log(const std::string& message){
+	time_t rawtime;
+	struct tm* timeinfo;
+	char buffer[80];
 
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	strftime(buffer, 80, "LOG | %d-%h-%G %T - ", timeinfo);
+
+	std::cout << "\033[1;32m" << buffer << message << "\033[m" << std::endl;
 }
 
 void Logger::Err(const std::string& message){
-	
+	time_t rawtime;
+	struct tm* timeinfo;
+	char buffer[80];
+
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	strftime(buffer, 80, "ERR | %d-%h-%G %T - ", timeinfo);
+
+	std::cout << "\033[1;31m" << buffer << message << "\033[m" << std::endl;
 }
