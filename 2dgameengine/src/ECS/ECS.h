507dd655208804e30a8ec04f79249f6d546b8c76
Felipe Fagua <felipe.fagua@unity3d.com>
2025-04-02 08:22:45 -0500

    Component Type Template

--- 2dgameengine/src/ECS/ECS.h
+++ 2dgameengine/src/ECS/ECS.h
@@ -16,8 +16,20 @@ typedef std::bitset<MAX_COMPONENTS> Signature;
 ////////////////////////////////////////////////////////////////////////////////
 // Component
 ////////////////////////////////////////////////////////////////////////////////
-class Component {
-	// TODO:...
+struct IComponent
+{
+	protected:
+		static int nextId;	
+};
+
+// Used to assign a unique id to a compolnent type
+template <typename T>
+class Component : public IComponent {
+	// Returns the unique id of Component<T>
+	static int GetId() {
+		static auto id = nextId++;
+		return id;
+	}
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -52,6 +64,10 @@ class System {
 		void RemoveEntityFromSystem();
 		std::vector<Entity> GetSystemEntities() const;
 		const Signature& GetComponentSignature() const;
+
+		// Define the component type T that entities must have to be
+		// considered by the system
+		template <typename TComponent> void RequireComponent();
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -60,5 +76,13 @@ class System {
 class Registry {
 	// TODO:...
 };
+
+// Implementations of the function template
+template <typename TComponent> 
+void System::RequireComponent()
+{
+	const auto componentId = Component<TComponent>::GetId();
+	componentSignature.set(componentId);
+}
 
 #endif
